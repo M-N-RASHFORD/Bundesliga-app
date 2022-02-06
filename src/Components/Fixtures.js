24f8ff22fa8764965a886183d58e5522a427d2f6
M-N-RASHFORD <85489724+M-N-RASHFORD@users.noreply.github.com>
2022-02-06 12:53:04 +0300

    modifying the fixture component

--- src/Components/Fixtures.js
+++ src/Components/Fixtures.js
@@ -1,19 +1,26 @@
-import React from 'react';
+import React from "react";
 
 //const apikey = 'c94495b0-7d0d-11ec-a59f-63c4906104b2';
 const Fixtures = () => {
-    React.useEffect(() => {
-        fetch('https://app.sportdataapi.com/api/v1/soccer/stages/1?apikey=apikey')
-        .then(resp => resp.json())
-        .then((data) => {
-            console.log(data);
-        })
-    }, []);
-    return (
-        <div>
-            <h2>Bundesliga fixtures for 2021/22 season</h2>
-        </div>
-    );
-}
+  const [fixtures, setFixtures] = React.useState([]);
+  React.useEffect(() => {
+    fetch("https://app.sportdataapi.com/api/v1/soccer/stages/1?apikey=apikey")
+      .then((resp) => resp.json())
+      .then((data) => {
+        console.log(data);
+        setFixtures(data);
+      });
+  }, []);
+  return (
+    <div>
+      <h2>Bundesliga fixtures for 2021/22 season</h2>
+      <h3>
+        {fixtures.map((fixture) => (
+          <li key={fixture.id}>{fixture.error}</li>
+        ))}
+      </h3>
+    </div>
+  );
+};
 
 export default Fixtures;
